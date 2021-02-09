d5914145e70f9c92c29442d222dd80f447161c16
Minas Katsiokalis <minaskatsiokalis@gmail.com>
2021-02-09 23:09:08 +0200

    Create sceneLoad.cs

---
+++ Scripts/sceneLoad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneLoad : MonoBehaviour {
+
+	public void startGameRacer()
+	{
+		SceneManager.LoadScene("GameRacer");
+	}
+
+	public void quitGameRacer()
+	{
+		Application.Quit();	
+	}
+
+}	
+
