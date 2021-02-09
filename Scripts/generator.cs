401f7e19ef30b07dff5f645e6a0cf40f3038c016
Minas Katsiokalis <minaskatsiokalis@gmail.com>
2021-02-09 23:08:43 +0200

    Create generator.cs

---
+++ Scripts/generator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generator : MonoBehaviour {
+	public GameObject stage;
+	private Vector3 stagePos;
+
+	private void Start(){
+		//Debug.Log("Creating stage");
+		stagePos = stage.transform.position;
+		stagePos = new Vector3(stagePos.x-62, stagePos.y, stagePos.z);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.name == "player_ball") {
+			Debug.Log("Creating stage");
+			Instantiate (stage,stagePos,Quaternion.identity);
+		}
+	}
+}
