decb419d34a2d4c681c772c1425c8e27cc710745
Minas Katsiokalis <minaskatsiokalis@gmail.com>
2021-02-09 23:08:34 +0200

    Create EnemyChecker.cs

---
+++ Scripts/EnemyChecker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChecker : MonoBehaviour { 
+	// Use this for initialization
+	private int tmp;
+	public GameObject ai_player;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerExit(Collider col){
+		if (col.gameObject.name == "player_ball") {
+			tmp = ai_player.GetComponent<EnemiesActions>().score;
+			ai_player.GetComponent<EnemiesActions>().score = col.gameObject.GetComponent<PlayerActions>().score ;
+			col.gameObject.GetComponent<PlayerActions>().score = tmp;
+		}	
+		else if (col.gameObject.name == "Enemy ball" ) {
+			tmp = ai_player.GetComponent<EnemiesActions>().score;
+			if (ai_player.GetComponent<EnemiesActions> ().score < col.gameObject.GetComponent<EnemiesActions> ().score) {
+				ai_player.GetComponent<EnemiesActions> ().score = col.gameObject.GetComponent<EnemiesActions> ().score;
+				col.gameObject.GetComponent<EnemiesActions>().score = tmp;
+			}
+		}	
+	}
+}
