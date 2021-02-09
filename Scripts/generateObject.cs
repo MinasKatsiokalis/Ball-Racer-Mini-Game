3ee4922126a4fe023e90436246845ebc19934693
Minas Katsiokalis <minaskatsiokalis@gmail.com>
2021-02-09 23:08:39 +0200

    Create generateObject.cs

---
+++ Scripts/generateObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generateObject : MonoBehaviour {
+
+	public GameObject[] obj;
+	private Vector3 objPos;
+	// Use this for initialization
+	void Start () {
+		int randomInt = Random.Range (0,obj.Length);
+		//sDebug.Log (randomInt);
+		objPos = this.transform.position;
+		Instantiate (obj[randomInt],objPos,Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
